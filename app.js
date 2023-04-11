cb30c5c3571542c3b94cc1c6eb20878e4dc33469
Calvin Crosby <calvincrosby@Calvins-Air.mshome.net>
2023-04-11 14:17:37 -0400

    a9 work done on node-server-app

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ import UserController
     from "./controllers/users/users-controller.js";
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
+
+mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
 
 const app = express()
 app.use(cors());
@@ -13,6 +16,7 @@ app.use(express.json());
 HelloController(app);
 UserController(app);
 TuitsController(app);
+
 
 app.listen(process.env.PORT || 4000);
 
